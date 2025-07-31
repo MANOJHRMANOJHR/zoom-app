39c3b1d33e4fda8db10ee5a5cf917de079c634cc
MANOJHRMANOJHR <manojhr799@gmail.com>
2025-07-31 23:13:56 +0530

    mongodbconnectionupdate

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const { createioserver } = require('./controllers/socket');
 
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const cook = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const http = require('http');
 const port = 3000;
@@ -30,6 +31,37 @@ app.use(
 
 app.use('/', require('./routers/userroutes'));
 
+
+
+
+
+const uri = process.env.dbhost;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+/*
 mongoose.connect(process.env.dbhost).then((res) => { //database creation using connect /myzoomdatabase
     console.log("Connected to the database ", res.connection.host);
 }
@@ -37,7 +69,7 @@ mongoose.connect(process.env.dbhost).then((res) => { //database creation using c
     console.log("Error connecting to the database", err);
 }
 );
-
+*/
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
